8b85ac4809e64d11ecadbbe6b776c9b628e20dae
youshush <yousephahmed1@gmail.com>
2026-01-21 19:57:36 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,6 @@
-import java.util.Scanner;
-
 public class Main { 
     public static void main(String[] args) {
-
         System.out.println("Hello World!");
-        
-        System.out.println( "Enter your name:");
-        Scanner name = new Scanner(System.in);
-        System.out.println("hello" + name.nextLine());
-
     }
 
     
